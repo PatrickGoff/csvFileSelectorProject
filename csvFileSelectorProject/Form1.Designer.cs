830a035c55c527012381136cf07b37fd2a16754e
PatrickGoff <patrickgoff4@gmail.com>
2022-08-18 11:42:07 -0500

    Add project files.

---
+++ csvFileSelectorProject/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace csvFileSelectorProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFileSelect = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFileSelect
+            // 
+            this.btnFileSelect.BackColor = System.Drawing.SystemColors.Info;
+            this.btnFileSelect.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnFileSelect.Location = new System.Drawing.Point(269, 96);
+            this.btnFileSelect.Name = "btnFileSelect";
+            this.btnFileSelect.Size = new System.Drawing.Size(190, 61);
+            this.btnFileSelect.TabIndex = 1;
+            this.btnFileSelect.Text = "Select File";
+            this.btnFileSelect.UseVisualStyleBackColor = false;
+            this.btnFileSelect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblResults.Location = new System.Drawing.Point(156, 240);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(113, 40);
+            this.lblResults.TabIndex = 2;
+            this.lblResults.Text = "Final 10 Digits \r\n     of Sum";
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(275, 253);
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(374, 27);
+            this.txtResults.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.btnFileSelect);
+            this.Name = "Form1";
+            this.Text = "CVC File Selector";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnFileSelect;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
---
+++ csvFileSelectorProject/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace csvFileSelectorProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Excel objects
+        Excel.Application xlApp = new Excel.Application();
+        Excel.Workbook xlWorkbook;
+        Excel.Worksheet xlWorksheet;
+        string sFileName;
+        int iRow, iCol = 1;
+        double sum = 0;
+        
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "CSV files | *.csv";
+            dialog.Multiselect = false;
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                sFileName = dialog.FileName; 
+
+                if(sFileName.Trim() != "")
+                {
+                    ReadExcel(sFileName);
+                }
+            }
+        }
+   
+    
+        private void ReadExcel(string sFileName)
+        {
+            xlApp = new Excel.Application();
+            xlWorkbook = xlApp.Workbooks.Open(sFileName);
+            xlWorksheet = xlApp.Worksheets["ExampleCSV"];
+
+            for (iRow = 1; iRow <= xlWorksheet.Rows.Count; iRow++)  // START FROM THE FIRST ROW.
+            {
+                if (xlWorksheet.Cells[iRow, 1].value == null)
+                {
+                    break;      // BREAK LOOP.
+                }
+                else
+                {
+                    
+                    txtResults.Text = sum.ToString();
+                }
+            }
+
+            xlWorkbook.Close();
+            xlApp.Quit();
+
+
+        }
+    
+    
+    
+    
+    
+    }
+}
